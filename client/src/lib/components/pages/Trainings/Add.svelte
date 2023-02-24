6f923240a46d8828f6c4e90d46df04069c3df8bb
Gunko Gleb <gynyax5@gmail.com>
2023-02-24 23:31:57 +0300

    some fixes + trainings loader

---
+++ client/src/lib/components/pages/Trainings/Add.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { _ } from 'svelte-i18n';
+    import ButtonBack from '../../common/buttonBack.svelte';
+    import TrainingHistory from '../../../testData/TrainingHistory.json'
+</script>
+
+<h1>{$_('trainings.addTitle')}</h1>
+<ButtonBack/>
+
+<!--
+    Название тренировки
+    Выбрать упражнение из стандартных и своих
+    Добавить свое упражнение
+
+
+-->
