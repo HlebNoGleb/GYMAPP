2e39df9eedcc5657db588339340ee5cc2355de9c
Gunko Gleb <gynyax5@gmail.com>
2024-07-13 14:58:37 +0300

    remove node

--- server/config.js
+++
@@ -1,7 +0,0 @@
-var config = {};
-
-config.db = {};
-
-config.db.path = "mongodb://127.0.0.1:27017/gymapp";
-
-module.exports = config;
--- server/routes/api.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-require("../app.js")
-
-module.exports = router;
