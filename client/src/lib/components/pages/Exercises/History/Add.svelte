e42cc6c6812d35e52e1d6f51055d54b69fcf7941
Gunko Gleb <gynyax5@gmail.com>
2024-12-21 02:15:37 +0300

    add timer

--- client/src/lib/components/pages/Exercises/History/Add.svelte
+++ client/src/lib/components/pages/Exercises/History/Add.svelte
@@ -5,6 +5,8 @@
     import storage from "../../../../helpers/storage/storage";
     import { HistoryModel } from '../../../../helpers/storage/Exercises/History/history';
     import { ExerciseType } from '../../../../helpers/storage/Exercises/exercises';
+    import { IntervalTimer, currentTimer } from '../../../../helpers/dateTime';
+    import { get, writable } from 'svelte/store';
 
     let exerciseId = $currentRouteData.exercise.id;
     //console.log($currentRouteData.exercise.type);
@@ -27,6 +29,66 @@
             alert("что-то не записал")
         }
 	}
+
+    enum TimerState {
+        start = 0,
+        pause = 1,
+        resume = 2,
+        clear = 3
+    }
+
+
+    // debugger
+    let currentTimerStart = localStorage.getItem(`timer-${exerciseId}`);
+    let currentTimerTime = (new Date().getTime() - Number(currentTimerStart ?? new Date().getTime())) / 1000;
+
+    console.log(currentTimerStart, currentTimerTime);
+
+    currentTimer.set(currentTimerTime);
+
+    let timerState = currentTimerStart ? TimerState.resume : TimerState.clear;
+
+    console.log(get(currentTimer));
+
+    let interval = new IntervalTimer(() => {
+        console.log(get(currentTimer));
+        currentTimer.update(value => value + 1);
+        newHistory.timer = new Date(get(currentTimer) * 1000).toISOString().slice(11, 19);
+    }, 1000);
+
+    runTimer(timerState);
+
+    console.log(interval)
+
+    function runTimer(state) {
+        console.log(state)
+        switch (state) {
+            case TimerState.start:
+                timerState = TimerState.start;
+                interval.start();
+                newHistory.timer = new Date(get(currentTimer) * 1000).toISOString().slice(11, 19);
+                localStorage.setItem(`timer-${exerciseId}`, new Date().getTime().toString());
+                break;
+            case TimerState.pause:
+                timerState = TimerState.pause;
+                interval.pause();
+                localStorage.setItem(`timer-${exerciseId}`, new Date(new Date().setSeconds(-get(currentTimer))).getTime().toString());
+                break;
+            case TimerState.resume:
+                timerState = TimerState.resume;
+                interval.pause();
+                interval.resume();
+                newHistory.timer = new Date(get(currentTimer) * 1000).toISOString().slice(11, 19);
+                break;
+            case TimerState.clear:
+                timerState = TimerState.clear;
+                interval.clear();
+                currentTimer.set(0);
+                newHistory.timer = new Date(get(currentTimer) * 1000).toISOString().slice(11, 19);
+                localStorage.removeItem(`timer-${exerciseId}`);
+                break;
+        }
+    }
 </script>
 
 <h1>Добавление истории {$currentRouteData.exercise.default ? $currentRouteData.exercise.name[$locale] : $currentRouteData.exercise.name}</h1>
@@ -64,9 +126,27 @@
 </div>
 {/if}
 <div class="mb-3">
-    <label for="sets" class="form-label">Дата и время</label>
+    <label for="date" class="form-label">Дата и время</label>
     <input type="datetime-local" bind:value={date} class="form-control" id="date" placeholder="1">
 </div>
+<div class="row mb-3 g-2">
+    <div class="col-auto flex-fill">
+        <label for="timer" class="form-label">Таймер</label>
+        <input type="time" step="1" bind:value={newHistory.timer} class="form-control" id="timer" placeholder="1">
+    </div>
+    <div class="d-flex col-auto align-items-end justify-content-end">
+        {#if timerState == TimerState.clear}
+            <button class="btn btn-primary" on:click={() => runTimer(TimerState.start)}><i class="fa fa-play"></i></button>
+        {:else if timerState == TimerState.start || timerState == TimerState.resume}
+            <button class="btn btn-primary me-1" on:click={() => runTimer(TimerState.pause)}><i class="fa fa-pause"></i></button>
+            <button class="btn btn-primary" on:click={() => runTimer(TimerState.clear)}><i class="fa fa-stop"></i></button>
+        {:else if timerState == TimerState.pause}
+            <button class="btn btn-primary me-1" on:click={() => runTimer(TimerState.resume)}><i class="fa fa-play"></i></button>
+            <button class="btn btn-primary" on:click={() => runTimer(TimerState.clear)}><i class="fa fa-stop"></i></button>
+        {/if}
+    </div>
+</div>
+
 <div class="mb-3">
     <label for="note" class="form-label">Описание упражнения</label>
     <textarea bind:value={newHistory.note} class="form-control" id="note" placeholder="Изи-пизи, можно повышать"></textarea>
--- client/src/lib/helpers/dateTime.js
+++ client/src/lib/helpers/dateTime.js
@@ -1,4 +1,4 @@
-// @ts-nocheck
+import { writable } from "svelte/store";
 /**
  * @param {Number} date
  */
@@ -196,3 +196,58 @@ export let getYearsList = () => {
 
     return yearList;
 }
+
+export class IntervalTimer {
+    callbackStartTime;
+    remaining = 0;
+    paused = false;
+    timerId = null;
+    _callback;
+    _delay;
+
+    constructor(callback, delay) {
+        this._callback = callback;
+        this._delay = delay;
+    }
+
+    pause() {
+        if (!this.paused) {
+            this.clear();
+            this.remaining = new Date().getTime() - this.callbackStartTime;
+            this.paused = true;
+        }
+    }
+
+    resume() {
+        if (this.paused) {
+            if (this.remaining) {
+                setTimeout(() => {
+                    this.run();
+                    this.paused = false;
+                    this.start();
+                }, this.remaining);
+            } else {
+                this.paused = false;
+                this.start();
+            }
+        }
+    }
+
+    clear() {
+        clearInterval(this.timerId);
+    }
+
+    start() {
+        this.clear();
+        this.timerId = setInterval(() => {
+            this.run();
+        }, this._delay);
+    }
+
+    run() {
+        this.callbackStartTime = new Date().getTime();
+        this._callback();
+    }
+}
+
+export let currentTimer = writable();
